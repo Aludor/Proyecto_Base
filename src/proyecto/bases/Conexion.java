8da55643d9fda8613cde6496c4d0ebddf8f7d5ee
Aludor <manuelvillagran@hotmail.com>
2019-03-15 22:23:30 -0600

    Inicio Proyecto

---
+++ src/proyecto/bases/Conexion.java
@@ -0,0 +1,34 @@
+package proyecto.bases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    Connection cx;
+    String db = "surtifiestas";
+    String url = "jdbc:mariadb://localhost:3306/" + db;
+    String user = "root";
+    String pass = "bases1";
+
+    public Connection conectar() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            cx = (Connection) DriverManager.getConnection(url, user, pass);
+            System.out.println("Conexion exitosa");
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("No se pudo conectar " + ex);
+        }
+        return cx;
+    }
+
+    public Connection desconectar() {
+        try {
+            cx.close();
+            System.out.println("Conexion terminada");
+        } catch (SQLException ex) {
+            System.out.println("No se pude cerrar la conexion");
+        }
+        return cx;
+    }
+}
